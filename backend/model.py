b85ba41611b6f80671154d2cefe566331e715ada
Abeehimr <ibraheemkureshi@gmail.com>
2026-06-07 15:37:45 +0500

    added: sqlalchemy db

---
+++ backend/model.py
@@ -0,0 +1,14 @@
+from backend.db import Base
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, text
+from sqlalchemy.sql import func
+
+
+class urlmap(Base):
+    __tablename__ = "urlmap"
+
+    id = Column(Integer,primary_key=True,nullable=False,autoincrement=True)
+    url = Column(String,nullable=False)
+    shortcode = Column(String,nullable=False,unique=True,index=True)
+    accessCount = Column(Integer,nullable=False,default=0)
+    createdAt = Column(DateTime, nullable=False, default=func.now())
+    updatedAt = Column(DateTime, nullable=False, default=func.now(),onupdate=func.now())
--- backend/validation_model.py
+++ backend/validation_model.py
@@ -3,6 +3,3 @@ from pydantic import BaseModel
 
 class LongUrl(BaseModel):
     url: str
-
-
-
