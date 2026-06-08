79958503527a4cca53880df11c5a5d416818d633
Abeehimr <ibraheemkureshi@gmail.com>
2026-06-08 16:47:10 +0500

    backend deployment

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from backend.app import app  # Vercel imports this directly
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("backend.app:app", host="0.0.0.0", port=8000, reload=True)
